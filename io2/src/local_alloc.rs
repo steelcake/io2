2955b18e53b419f0d01bcf2b9f389f69a3d272ed
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-10-08 06:26:36 +0600

    save

--- io2/src/local_alloc.rs
+++ io2/src/local_alloc.rs
@@ -6,9 +6,6 @@ use std::{
     ptr::NonNull,
 };
 
-const ONE_GB: usize = 1024 * 1024 * 1024;
-const TWO_MB: usize = 2 * 1024 * 1024;
-
 thread_local! {
     static PAGES: RefCell<State> = RefCell::new(State::new());
 }
@@ -19,8 +16,6 @@ struct State {
     pages: Vec<Page>,
     free_list: Vec<Vec<FreeRange>>,
 }
-
-const HUGE_PAGE_SIZE_ENV_VAR_NAME: &str = "LOCAL_ALLOC_HUGE_PAGE_SIZE";
 
 impl State {
     fn new() -> Self {
@@ -85,101 +80,15 @@ impl LocalAlloc {
 
 unsafe impl Allocator for LocalAlloc {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        PAGES.with_borrow_mut(|pages| {
-            for page in pages.iter_mut() {
-                let mut alloc = None;
-                for (page_idx, range) in page.free_list.iter().enumerate() {
-                    let alloc_start =
-                        (page.ptr as usize + range.start).next_multiple_of(layout.align());
-                    let alloc_start = alloc_start - page.ptr as usize;
-                    if alloc_start + layout.size() > range.start + range.len {
-                        continue;
-                    }
+        if layout.align() > TWO_MB {
+            panic!("alignment can't be bigger than 2MB");
+        }
 
-                    alloc = Some((page_idx, alloc_start));
-                    break;
-                }
-
-                if let Some(alloc) = alloc {
-                    let mut range = *page.free_list.get(alloc.0).unwrap();
-                    if alloc.1 == range.start {
-                        range.start += layout.size();
-                        range.len -= layout.size();
-                    } else {
-                        let new_len = alloc.1 - range.start;
-                        page.free_list.push(FreeRange {
-                            start: alloc.1 + layout.size(),
-                            len: range.len - new_len - layout.size(),
-                        });
-                        range.len = new_len;
-                    }
-                    page.free_list[alloc.0] = range;
-
-                    unsafe {
-                        return Ok(NonNull::new_unchecked(std::ptr::slice_from_raw_parts_mut(
-                            page.ptr.add(alloc.1),
-                            layout.size(),
-                        )));
-                    }
-                }
-            }
-
-            let align = ALIGN.next_multiple_of(layout.align());
-            let size = ALIGN.next_multiple_of(layout.size());
-            let page_layout = Layout::from_size_align(size, align).unwrap();
-            let ptr = unsafe { std::alloc::alloc(page_layout) };
-            let mut free_list = Vec::with_capacity(32);
-            free_list.push(FreeRange {
-                start: layout.size(),
-                len: size - layout.size(),
-            });
-
-            pages.push(Page {
-                ptr,
-                layout: page_layout,
-                free_list,
-            });
-
-            unsafe {
-                Ok(NonNull::new_unchecked(std::ptr::slice_from_raw_parts_mut(
-                    ptr,
-                    layout.size(),
-                )))
-            }
-        })
+        todo!()
     }
 
     unsafe fn deallocate(&self, ptr: std::ptr::NonNull<u8>, layout: Layout) {
-        let ptr = ptr.as_ptr();
-        PAGES.with_borrow_mut(|pages| {
-            for page in pages.iter_mut() {
-                if page.ptr <= ptr && page.ptr.add(page.layout.size()) >= ptr.add(layout.size()) {
-                    let start = ptr.sub(page.ptr as usize) as usize;
-                    let end = start + layout.size();
-                    let mut found = false;
-                    dbg!((start, end, page.layout.size()));
-                    for range in page.free_list.iter_mut() {
-                        if start == range.start + range.len {
-                            range.len += layout.size();
-                            found = true;
-                        } else if end == range.start {
-                            range.start -= layout.size();
-                            found = true;
-                        }
-                    }
-                    if !found {
-                        page.free_list.push(FreeRange {
-                            start: start,
-                            len: layout.size(),
-                        });
-                    }
-
-                    return;
-                }
-            }
-
-            panic!("bad deallocate");
-        })
+        todo!()
     }
 }
 
@@ -245,6 +154,10 @@ unsafe fn free(ptr: *mut u8, length: usize) -> io::Result<()> {
         )),
     }
 }
+
+const ONE_GB: usize = 1024 * 1024 * 1024;
+const TWO_MB: usize = 2 * 1024 * 1024;
+const HUGE_PAGE_SIZE_ENV_VAR_NAME: &str = "LOCAL_ALLOC_HUGE_PAGE_SIZE";
 
 #[cfg(test)]
 mod tests {
