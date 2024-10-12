ea1eb0644c2dbff474eae6c20fa188c394276ea4
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-10-12 17:00:40 +0200

    clean-up dio read

--- io2/src/fs/dio_file.rs
+++ io2/src/fs/dio_file.rs
@@ -56,36 +56,33 @@ impl DioFile {
     }
 
     pub fn read<A: Allocator + Unpin + Copy>(&self, offset: u64, size: usize, alloc: A) -> Read<A> {
-        let read_offset = align_down(offset, u64::from(self.dio_offset_align));
-        let read_size = align_up(u32::try_from(size).unwrap(), self.dio_offset_align);
-        let view_start = usize::try_from(offset.checked_sub(read_offset).unwrap()).unwrap();
-
-        Read {
+        Read::Start {
             file: self,
-            read_offset,
-            read_size,
-            mem_align: usize::try_from(self.dio_mem_align).unwrap(),
-            view_start,
-            view_len: size,
+            offset,
+            size,
             alloc,
-            io_id: None,
-            buf: None,
             _non_send: PhantomData,
         }
     }
 }
 
-pub struct Read<'file, A: Allocator + Unpin + Copy> {
-    file: &'file DioFile,
-    read_offset: u64,
-    read_size: u32,
-    mem_align: usize,
-    view_start: usize,
-    view_len: usize,
-    alloc: A,
-    io_id: Option<slab::Key>,
-    buf: Option<IoBuffer<A>>,
-    _non_send: PhantomData<*mut ()>,
+pub enum Read<'file, A: Allocator + Unpin + Copy> {
+    Start {
+        file: &'file DioFile,
+        offset: u64,
+        size: usize,
+        alloc: A,
+        _non_send: PhantomData<*mut ()>,
+    },
+    Wait {
+        file: &'file DioFile,
+        io_id: slab::Key,
+        buf: IoBuffer<A>,
+        view_start: usize,
+        view_len: usize,
+        _non_send: PhantomData<*mut ()>,
+    },
+    Empty,
 }
 
 impl<'file, A: Allocator + Unpin + Copy> Future for Read<'file, A> {
@@ -95,26 +92,31 @@ impl<'file, A: Allocator + Unpin + Copy> Future for Read<'file, A> {
         CURRENT_TASK_CONTEXT.with_borrow_mut(|ctx| {
             let ctx = ctx.as_mut().unwrap();
             let fut = self.get_mut();
-            match fut.io_id {
-                None => {
+            match std::mem::replace(fut, Read::Empty) {
+                Read::Start { file, offset, size, alloc, ..} => {
+                    let read_offset = align_down(offset, u64::from(file.dio_offset_align));
+                    let read_size = align_up(u32::try_from(size).unwrap(), file.dio_offset_align);
+                    let view_start = usize::try_from(offset.checked_sub(read_offset).unwrap()).unwrap();
+                    let view_len = size;
+
                     let layout = Layout::from_size_align(
-                        usize::try_from(fut.read_size).unwrap(),
-                        fut.mem_align,
+                        usize::try_from(read_size).unwrap(),
+                        usize::try_from(file.dio_mem_align).unwrap(),
                     )
                     .unwrap();
-                    let mut buf = IoBuffer::new(layout, fut.alloc).unwrap();
+                    let mut buf = IoBuffer::new(layout, alloc).unwrap();
                     let buf_ptr = buf.as_mut_ptr();
-                    fut.buf = Some(buf);
-                    fut.io_id = Some(unsafe {
+                    let io_id = unsafe {
                         ctx.queue_dio(
-                            opcode::Read::new(Fd(fut.file.file.fd), buf_ptr, fut.read_size)
-                                .offset(fut.read_offset)
+                            opcode::Read::new(Fd(file.file.fd), buf_ptr, read_size)
+                                .offset(read_offset)
                                 .build(),
                         )
-                    });
+                    };
+                    *fut = Read::Wait { file, io_id, buf, view_start, view_len, _non_send: PhantomData };
                     Poll::Pending
                 }
-                Some(io_id) => {
+                Read::Wait { io_id, buf, view_start, view_len, .. } => {
                     let io_result = match ctx.take_io_result(io_id) {
                         Some(io_result) => io_result,
                         None => {
@@ -122,15 +124,16 @@ impl<'file, A: Allocator + Unpin + Copy> Future for Read<'file, A> {
                         }
                     };
 
+                    *fut = Read::Empty;
                     if io_result < 0 {
                         Poll::Ready(Err(io::Error::from_raw_os_error(-io_result)))
                     } else {
                         let n_read = usize::try_from(io_result).unwrap();
-                        let buf = fut.buf.take().unwrap();
-                        let view = IoBufferView::new(buf, fut.view_start, fut.view_len.min(n_read));
+                        let view = IoBufferView::new(buf, view_start, view_len.min(n_read));
                         Poll::Ready(Ok(view))
                     }
                 }
+                Read::Empty => unreachable!(),
             }
         })
     }
