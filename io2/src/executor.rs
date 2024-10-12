a8985f8caf9dcffb2aaa32b4f0bc2dc9cc0209c0
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-10-13 05:49:39 +0600

    fairer scheduling

--- io2/src/executor.rs
+++ io2/src/executor.rs
@@ -219,7 +219,7 @@ fn run<T: 'static, F: Future<Output = T> + 'static>(
     let mut io_results =
         IoResults::with_capacity_in(usize::try_from(ring_depth).unwrap() * 4, LocalAlloc::new());
     let mut to_notify = ToNotify::with_capacity_in(128, LocalAlloc::new());
-    let mut notifying = VecDeque::<slab::Key, LocalAlloc>::with_capacity_in(128, LocalAlloc::new());
+    let mut notifying = Vec::<slab::Key, LocalAlloc>::with_capacity_in(128, LocalAlloc::new());
 
     let close_file_task_id = tasks.insert(Box::pin_in(async {}, LocalAlloc::new()));
     let close_file_io_id = io.insert(close_file_task_id);
@@ -272,7 +272,7 @@ fn run<T: 'static, F: Future<Output = T> + 'static>(
         if !to_notify.is_empty() {
             notifying.extend(to_notify.iter().map(|kv| kv.0));
             to_notify.clear();
-            while let Some(task_id) = notifying.pop_front() {
+            while let Some(task_id) = notifying.pop() {
                 let task_start = Instant::now();
                 CURRENT_TASK_CONTEXT.with_borrow_mut(|ctx| {
                     *ctx = Some(CurrentTaskContext {
